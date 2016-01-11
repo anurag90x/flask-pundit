e44417c817bf582c338ea2a1ca9d9b3290b383ce
anurag90x <anuragchaudhury@gmail.com>
2016-01-11 12:52:13 -0500

    Cleanup the main file a bit

--- flask_pundit/flask_pundit.py
+++ flask_pundit/flask_pundit.py
@@ -1,24 +1,20 @@
-def authorize(record, action=get_action_from_request, user=get_current_user):
-    current_user, action, policy_clazz = _get_parameters(user, action)
+import flask
+
+def authorize(record, action=None, user=None):
+    current_user = user or _get_current_user()
+    action = action or _get_action_from_request()
+    policy_clazz = _get_policy_clazz(record)
+
     if policy_clazz is not None:
         return getattr(policy_clazz(current_user, record), action)()
-    else:
-        raise RunTimeError('Required policy class for type %s' % record)
+    raise RuntimeError('Required policy class for type %s' % record)
 
-def get_current_user():
+def _get_current_user():
     return flask.g.get('user') or flask.g.get('current_user')
 
-def get_policy_clazz(record):
+def _get_policy_clazz(record):
     record_clazz = getattr(record, '__class__')
-    if record_clazz:
-        policy_clazz = getattr(record_clazz, 'policy_class')
-        return policy_clazz
+    return getattr(record_clazz, 'policy_class') if record_clazz else None
 
-def get_action_from_request():
-    return request.method.lower()
-
-def _get_parameters(user, action):
-    current_user = user if !has_attr(user, '__call__') else user()
-    action = action if !has_attr(action, '__call__') else action()
-    policy_clazz = get_policy_clazz()
-    return (current_user, action, policy_clazz)
+def _get_action_from_request():
+    return flask.request.method.lower()
