1530c9d3fd84d9134975a029b833db1e0e0c2f3d
anurag90x <anuragchaudhury@gmail.com>
2016-01-13 03:54:08 -0500

    Add verification method support and decorators

--- flask_pundit/__init__.py
+++ flask_pundit/__init__.py
@@ -1,4 +1,33 @@
+from constants import PUNDIT_CALLBACKS
+from flask import _app_ctx_stack as stack
 import flask_pundit
+from functools import wraps
 
 authorize = flask_pundit.authorize
 policy_scope = flask_pundit.policy_scope
+
+
+def verify_authorized(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        stack.top[PUNDIT_CALLBACKS] = stack.top.get(PUNDIT_CALLBACKS, [])\
+                                               .append(flask_pundit.verify_authorized)
+        return func(*args, **kwargs)
+    return inner
+
+
+def verify_policy_scoped(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        stack.top[PUNDIT_CALLBACKS] = stack.top.get(PUNDIT_CALLBACKS, [])\
+                                               .append(flask_pundit.verify_policy_scoped)
+        return func(*args, **kwargs)
+    return inner
+
+
+@app.after_request
+def _process_verification_callbacks():
+    callbacks = stack.top.get(PUNDIT_CALLBACKS)
+    for call in callbacks:
+        if not call():
+            raise RuntimeError
--- flask_pundit/flask_pundit.py
+++ flask_pundit/flask_pundit.py
@@ -1,5 +1,6 @@
 import flask
 
+
 def authorize(record, action=None, user=None):
     """ Call this method from within a resource or
     a route to authorize a model instance
@@ -7,7 +8,12 @@ def authorize(record, action=None, user=None):
     current_user = user or _get_current_user()
     action = action or _get_action_from_request()
     policy_clazz = _get_policy_clazz(record)
-    return _delegate_action(policy_clazz, current_user, record, action)
+    action_response = _delegate_action(policy_clazz, current_user, record, action)
+
+    if policy_clazz is not None:
+        flask._app_ctx_stack.top[AUTHORIZE_CALLED] = True
+        return getattr(policy_clazz(current_user, record), action)()
+    raise RuntimeError('Required policy class for type %s' % record)
 
 def policy_scope(record, user=None):
     """ Call this method from within a resource or
@@ -17,7 +23,19 @@ def policy_scope(record, user=None):
     current_user = user or _get_current_user()
     action = 'resolve'
     scope_clazz = _get_scope_clazz(record)
-    return _delegate_action(scope_clazz, current_user, record, action)
+
+    if scope_clazz is not None:
+        flask._app_ctx_stack.top[POLICY_SCOPE_CALLED] = True
+        return getattr(scope_clazz(current_user, record), action)()
+    raise RuntimeError('Required policy class for type %s' % record)
+
+def verify_authorized():
+    return flask._app_ctx_stack.top.pop(AUTHORIZE_CALLED, False)
+
+
+def verify_policy_scoped():
+    return flask._app_ctx_stack.top.pop(POLICY_SCOPE_CALLED, False)
+
 
 def _delegate_action(clazz, user, record, action):
     """ Delegates the action to an instance of the class
@@ -28,16 +46,20 @@ def _delegate_action(clazz, user, record, action):
         return getattr(clazz(user, record), action)()
     raise RuntimeError('Required policy class for type %s' % record)
 
+
 def _get_current_user():
     return flask.g.get('user') or flask.g.get('current_user')
+
 
 def _get_policy_clazz(record):
     record_clazz = getattr(record, '__class__')
     return getattr(record_clazz, 'policy_class') if record_clazz else None
 
+
 def _get_scope_clazz(record):
     policy_clazz = _get_policy_clazz(record)
     return getattr(policy_clazz, 'Scope') if policy_clazz else None
+
 
 def _get_action_from_request():
     return flask.request.method.lower()
