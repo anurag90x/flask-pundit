a0a6c40a3441c61222d10af4ed1b6e4961eee32e
anurag90x <anuragchaudhury@gmail.com>
2016-01-18 12:30:36 -0500

    Fix setup.py

--- flask_pundit/application_policy.py
+++ flask_pundit/application_policy.py
@@ -1,10 +1,13 @@
 class ApplicationPolicy:
+    '''Base class for all policy'''
     def __init__(self, user, record):
         self.user = user
         self.record = record
+
     class Scope:
         def __init__(self, user, scope):
             self.user = user
             self.scope = scope
+
         def resolve(self):
             pass
--- setup.py
+++ setup.py
@@ -1,32 +1,29 @@
 from setuptools import setup, find_packages
-import sys
 
-PY26 = sys.version_info[:2] == (2, 6,)
 
 requirements = [
-    'Flask>=0.8',
+    'Flask==0.10.1',
 ]
 
 setup(
     name='Flask-Pundit',
-    version='0.0.1',
-    license='BSD',
+    version='1.0.0',
+    license='MIT',
     url='https://www.github.com/flask-pundit/flask-pundit/',
     author='Anurag Chaudhury',
     author_email='anuragchaudhury@gmail.com',
-    description='Simple library to manage permissions and scopes',
+    description='Simple library to manage resource authorization and scopoing',
     packages=find_packages(exclude=['tests']),
-    classifiers=[
-        'Framework :: Flask',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'License :: OSI Approved :: BSD License',
-    ],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
-    test_suite = 'nose.collector',
+    test_suite='nose.collector',
     install_requires=requirements,
-    tests_require=['mock>=0.8']
+    tests_require=['mock==1.3.0'],
+    classifiers=[
+        'Framework :: Flask',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License'
+    ]
 )
