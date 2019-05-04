ae18c73880316ce064c3e2f6cac7538872138cf4
Anurag Chaudhury <anuragchaudhury@gmail.com>
2019-05-04 13:41:06 -0400

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = [
 
 setup(
     name='flask-pundit',
-    version='1.1.0',
+    version='1.2.0',
     license='MIT',
     url='https://github.com/anurag90x/flask-pundit',
     author='Anurag Chaudhury',
